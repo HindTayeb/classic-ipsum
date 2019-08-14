f1db9c952979d2ad67a737c493ad57496a44b30b
39D■ <hpoje@icloud.com>
2019-08-14 20:34:24 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# مولد لوريم ابسوم باللغة العريبة - Arabic Lorem Ipsum Generator
+يولد جمل واقتباسات مختلفة من قصص كلاسيكية قديمة مثل: مغامرات شيرلوك هولمز، كبرياء وهوى، توم سوير وغيرها
+<br>Arabic Lorem Ipsum Generator that generates various qoutes and sentences from old classics like: the adventures of Sherolck Holmes, Pride and Prejudice, Tom Sawyer, and others.
+
+![](/Screenshot.png?raw=true)
+
+## الحقوق و المراجع - Credits and References
+
+- قد قمت باستعمال [هذه المقال](https://hackernoon.com/creating-a-lorem-ipsum-generator-with-node-and-express-9e1af0b31c86) لبناء 
+المولد
+- وقمت باستعمال [هذا القالب](https://startbootstrap.com/template-overviews/coming-soon/) من بوتستراب لتصميم الموقع
+- Used this [tutorial](https://hackernoon.com/creating-a-lorem-ipsum-generator-with-node-and-express-9e1af0b31c86) to build the base for Lorem Ipsum Generator.
+- Used this [bootstrap template](https://startbootstrap.com/template-overviews/coming-soon/) for css references
+
---
+++ app.js
@@ -0,0 +1,30 @@
+// Require express and create an express application instance
+const express = require('express');
+const app = express();
+const path = require('path');
+var server = require('http').Server(app);
+
+// Require the express routes defined in router.js
+const routes = require('./router');
+
+// Define the hostname and port where the server can be found
+const hostname = "127.0.0.1";
+
+const port =  process.env.PORT || 8080 // 3000;
+
+// Define the directory where static files are found
+app.use(express.static(__dirname + '/public'));
+
+const port = process.env.PUBLIC_PORT;
+
+// Define the directory where static files are found
+app.use(express.static(path.join(__dirname)));
+
+// Specify the routes to be used for our application 
+app.use(routes);
+
+// Begin accepting connections to the specified port
+app.listen(port, () => {
+  // Display server location information to the console
+  console.log(`Server is listening at http://${hostname}:${port}/`);
+})
